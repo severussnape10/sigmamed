0c2320140408885141d528f0153358ea2ab79151
@Gokul-Prakash2001 <gokulprakash9344@gmail.com>
2021-11-24 14:47:15 +0530

    updated admin.py

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Doctor,Patient,Appointment
 # Register your models here.
+class DoctorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Doctor, DoctorAdmin)
+
+class PatientAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Patient, PatientAdmin)
+
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Appointment, AppointmentAdmin)
+
+
